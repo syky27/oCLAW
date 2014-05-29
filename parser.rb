87360f3aaad1c3c3278c3883b0a124492d2bc3eb
jump@syky.cz <jump@syky.cz>
2014-05-29 21:05:15 +0200

    I can post a new file now :)

--- parser.rb
+++ parser.rb
@@ -2,7 +2,7 @@ require 'optparse'
 require 'base64'
 require 'socket'
 require 'net/http'
-
+require 'rest_client'
 
 my_ip = Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
 printer_ip = my_ip[0.. (my_ip.rindex('.'))]+ARGV[0]
@@ -13,12 +13,20 @@ File.open(ARGV[1], 'r') do|gcode|
 	encoded_gcode =  Base64.encode64(gcode.read)
 end
 
-puts uri = "http://" + printer_ip + ":5000/api/state?apikey=r3pr4pfit" 
-better_uri = URI(uri)
-puts better_uri
+puts url = "http://" + printer_ip + ":5000/api/state?apikey=r3pr4pfit" 
+puts upload_url = "http://" + printer_ip + ":5000/api/load?apikey=r3pr4pfit" 
+u = "http://172.16.60.123:5000/ajax/gcodefiles/upload"
+uri = URI(url)
+puts uri
 
-# uri = URI('http://example.com/index.html?count=10')
-puts Net::HTTP.get(better_uri) # => String
+
+RestClient.post u, :gcode_file => File.new(ARGV[1], 'r', :select => "true")
+
+
+#RestClient.post(u, File.new(ARGV[1]))
+# puts printer_state = Net::HTTP.get(uri) # => String
+
+# http://172.16.60.123:5000/ajax/gcodefiles/upload
 
 
 # http://localhost:5000/api/state?apikey=PUT_YOUR_API_KEY_HERE
